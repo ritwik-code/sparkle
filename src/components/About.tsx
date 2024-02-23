f5e05e84c1ea0d3c3114eade6a0350b370247fb5
Ritwik Mishra <ritwikmishra.dev@gmail.com>
2024-02-23 16:59:28 +0300

    Changing questions and copy

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,17 +1,20 @@
 function About() {
   return (
 
-      <div className='grid h-screen place-items-center'>
+      <div className='grid h-screen place-items-center pt-5'>
 
         <div className='w-3/4'>
         <div className="mt-5">
             <h1 className='text-center font-customFont font-outline-05 font-bold text-4xl text-white'>ABOUT</h1>
           </div>
           <div className="mt-5">
-            <h2 className='text-center font-customFont font-outline-05 font-bold text-xl text-white'>As a child, we all loved listening to our parents when they read a storybook, or when they shared the story of how they met for the very first time. Stories give us hope, excitement, and most importantly, they create a sense of belonging. Historically, storytelling served as a way for people to gather, to share inspiring experiences and sacred moments, to provide meaning to the world, and to connect. Through storytelling we come together and share our humanity.</h2>
+            <h2 className='text-center font-customFont font-outline-05 font-bold text-xl text-white'>As a child, we all loved listening to our parents when they read a storybook, or when they shared the story of how they met for the very first time. Stories give us hope, excitement, and most importantly, they create a sense of belonging.</h2>
           </div>
           <div className="mt-5">
-            <h2 className='text-center font-customFont font-outline-05 font-bold text-xl text-white'>The name Sparkle is inspired by the idea of shining brightly, with unique flashes of light representing personal experiences. Sharing your stories with others will enhance this sparkle and help you to see yourself and other team members in a new light. At its core, Sparkle is about building strong, long-lasting relationships and mutual support. We believe that by playing the game, you will not only have a good time with your colleagues, but you will also be better equipped to tackle challenges together and achieve better results.</h2>
+            <h2 className='text-center font-customFont font-outline-05 font-bold text-xl text-white'>The name Sparkle is inspired by the idea of shining brightly, with unique flashes of light representing personal experiences. Sharing your stories with others will enhance this sparkle and help you to see yourself and other team members in a new light. </h2>
+          </div>
+          <div className="mt-5">
+            <h2 className='text-center font-customFont font-outline-05 font-bold text-xl text-white'> At its core, Sparkle is about building strong, long-lasting relationships and mutual support. We believe that by playing the game, you will not only have a good time with your colleagues, but you will also be better equipped to tackle challenges together and achieve better results. </h2>
           </div>
           <div className="mt-5">
             <h2 className='text-center font-customFont font-outline-05 font-bold text-xl text-white'>Let us kindle the fire and Sparkle!</h2>
@@ -23,5 +26,7 @@ function About() {
       </div>
   );
 }
+
+
 
 export default About;
